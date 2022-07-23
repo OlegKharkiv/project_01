77a083d8b5c8c75628151bc62b453356bdf2440d
Oleg <dginm6@gmail.com>
2022-07-23 14:01:24 +0300

    first commit

---
+++ Lesson_01/script.js
@@ -0,0 +1,15 @@
+"use sctrict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here");
+//console.log(result);
+
+//const answer = prompt("How old are you", "18");
+//console.log(answer);
+
+const category = 'toys';
+console.log(`http://someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Hi there, ${user}`);
